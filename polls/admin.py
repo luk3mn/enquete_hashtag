e17f424340acdb69ad6074f069d246c724ce35c3
luk3mn <lucasrenan2015@gmail.com>
2022-06-23 19:54:37 -0300

    Trabalhando com o banco de dados e a interface de administrador

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Question
+
+# Registra na interface de admin o Question
+admin.site.register(Question)
--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from django.db import models
 
 # Create your models here.
@@ -6,7 +7,17 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date_published') # 'date_published' não é obrigatorio
 
+    # Para poder apresentar as 'Question' de forma mais específica
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE) # 'Question' referencia a classe 'Question', que será a tabela no sqlite
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0) # 'default=0' começa sempre no zero
+
+    def __str__(self):
+        return self.choice_text
