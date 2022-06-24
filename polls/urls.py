845651fbc7463e02babaa81bc123a83d5dc0191d
luk3mn <lucasrenan2015@gmail.com>
2022-06-24 20:36:35 -0300

    Criando novas views, páginas HTML e finalizando o sistema

--- polls/urls.py
+++ polls/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 # trás tudo que está no arquivo views
 from . import views
 
+app_name = 'polls'
 urlpatterns = [ # 'lista' de urls
     path('', views.index, name='index'), # chama de index a funcao index do views
+    # path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote')
 ]
